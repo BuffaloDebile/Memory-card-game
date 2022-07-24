601d0213b72553c83533b76a972d1c1746bbde55
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-07-24 21:11:51 +0200

    checking if card are the same and preventing spambug

--- app.js
+++ app.js
@@ -11,20 +11,38 @@ shuffleCards();
 
 cards.forEach((card) => card.addEventListener('click', flipACard));
 
+let lockedCards = false;
 let = cardsPicked = [];
 
 function flipACard(e) {
+  if (lockedCards) return;
+
   saveCard(e.target.children[0], e.target.getAttribute('data-attr'));
+
+  if (cardsPicked.length === 2) result();
+  
 }
-
-// if (cardPicked.lenght === 2 ) result() {
-
-// }
 
 function saveCard(el, value) {
   if (el === cardsPicked[0]?.el) return;
   el.classList.add('active');
-  cardsPicked.push(el, value);
+  cardsPicked.push({ el, value });
 }
 
-function result() {}
+function result() {
+//   saveNumberOfTries();
+  if (cardsPicked[0].value === cardsPicked[1].value) {
+    cardsPicked[0].el.parentElement.removeEventListener('click', flipACard);
+    cardsPicked[1].el.parentElement.removeEventListener('click', flipACard);
+    cardsPicked = [];
+    return;
+  }
+
+  lockedCards = true;
+  setTimeout(() => {
+    cardsPicked[0].el.classList.remove('active');
+    cardsPicked[1].el.classList.remove('active');
+    cardsPicked = [];
+    lockedCards = false;
+  }, 1000);
+}
