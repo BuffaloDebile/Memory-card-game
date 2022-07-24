e013f7e6915d726ba4c1e008b22d8fd5fbcd2116
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-07-24 22:23:43 +0200

    counting number of tries

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ function flipACard(e) {
   saveCard(e.target.children[0], e.target.getAttribute('data-attr'));
 
   if (cardsPicked.length === 2) result();
-  
 }
 
 function saveCard(el, value) {
@@ -30,7 +29,7 @@ function saveCard(el, value) {
 }
 
 function result() {
-//   saveNumberOfTries();
+  saveNumberOfTries();
   if (cardsPicked[0].value === cardsPicked[1].value) {
     cardsPicked[0].el.parentElement.removeEventListener('click', flipACard);
     cardsPicked[1].el.parentElement.removeEventListener('click', flipACard);
@@ -45,4 +44,23 @@ function result() {
     cardsPicked = [];
     lockedCards = false;
   }, 1000);
+}
+
+const innerCards = [...document.querySelectorAll('.double-face')];
+const advice = document.querySelector('.advice');
+const score = document.querySelector('.score');
+
+numberOfTries = 0;
+
+function saveNumberOfTries() {
+  numberOfTries++;
+  const checkForEnd = innerCards.filter(
+    (card) => !card.classList.contains('active'),
+  );
+  if (!checkForEnd.length) {
+    advice.textContent = `You won ! Press "space" to start again`;
+    score.textContent = `Final score : ${numberOfTries}`;
+    return;
+  }
+  score.textContent = `Number of tries: ${numberOfTries}`;
 }
